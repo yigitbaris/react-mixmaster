424337c295784ea55e6d6556110b2be5f552bc72
yigitbaris <bariskaynakoglu01@gmail.com>
2023-08-08 11:26:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import {
+  About,
+  Coctail,
+  Error,
+  HomeLayout,
+  Landing,
+  Newsletter,
+  SinglePageError,
+} from './pages'
+
+import { loader as landingLoader } from './pages/Landing'
+import { loader as singleCocktailLoader } from './pages/Coctail'
+import { action as newsletterAction } from './pages/Newsletter'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+})
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <SinglePageError />,
+        loader: landingLoader(queryClient),
+      },
+      {
+        path: 'coctail/:id',
+        errorElement: <SinglePageError />,
+        loader: singleCocktailLoader(queryClient),
+        element: <Coctail />,
+      },
+      {
+        path: 'newsletter',
+        element: <Newsletter />,
+        action: newsletterAction,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+    ],
+  },
+])
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+export default App
